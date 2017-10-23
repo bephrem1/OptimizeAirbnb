203d977de5bc3dbb0b6e91b1c364568d239eaf86
Benyam Ephrem <ephremdsg@gmail.com>
2017-10-23 01:12:04 -0400

    Initial File Setup, Dependency Setup, and Scaffolding.

---
+++ OptimizeAirbnb/src/main/java/com/benyamephrem/Application.java
@@ -0,0 +1,15 @@
+package com.benyamephrem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.data.mongo.MongoDataAutoConfiguration;
+import org.springframework.boot.autoconfigure.mongo.MongoAutoConfiguration;
+
+@SpringBootApplication
+public class Application{
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+
+}
