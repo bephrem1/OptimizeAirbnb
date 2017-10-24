c552e412f8c4cf7fa1b324faa7af751edeefc014
Benyam Ephrem <ephremdsg@gmail.com>
2017-10-24 18:45:14 -0400

    Feature 1/3 Functional: When Hompage is loaded a pie chart of neighborhoods and their respective listing count is shown. README file has more details about known bugs and errors

--- OptimizeAirbnb/src/main/java/com/benyamephrem/service/ListingServiceImpl.java
+++ OptimizeAirbnb/src/main/java/com/benyamephrem/service/ListingServiceImpl.java
@@ -2,12 +2,15 @@ package com.benyamephrem.service;
 
 import com.benyamephrem.dao.ListingDao;
 import com.benyamephrem.model.Listing;
+import com.benyamephrem.model.constants.Neighborhood;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+//TODO:be DATA SHOULD BE CACHED so we don't have to keep running expensive requests that have already been run. It's slowing application down badly.
 @Service
 public class ListingServiceImpl implements ListingService{
     @Autowired
@@ -29,11 +32,22 @@ public class ListingServiceImpl implements ListingService{
         return matchedListings.size();
     }
 
+    //TODO:be Is passing a map up to the controller the best idea? I think it consolidates things and keeps processing in service layer...revise
     @Override
     public Map<String, Integer> getNeighborhoodToListingCountMap() {
+        //Initialize map to return
+        Map<String, Integer> neighborhoodToCountMap = new HashMap<>();
 
+        //Get all neighborhoods to get count of from our enum
+        Neighborhood[] neighborhoods = Neighborhood.class.getEnumConstants();
 
-        return null;
+        //Populate the map to be returned
+        for (Neighborhood n : neighborhoods){
+            //Add keys to the map and get the count of listings that neighborhood has
+            neighborhoodToCountMap.put(n.getName(), getListingCountForNeighborhood(n.getName()));
+        }
+
+        return neighborhoodToCountMap;
     }
 
 }
--- OptimizeAirbnb/src/main/java/com/benyamephrem/web/controller/ListingController.java
+++ OptimizeAirbnb/src/main/java/com/benyamephrem/web/controller/ListingController.java
@@ -6,6 +6,8 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import java.util.Map;
+
 @Controller
 public class ListingController {
 
@@ -14,8 +16,13 @@ public class ListingController {
 
     @RequestMapping("/")
     public String getHomepage(Model model){
+        Map<String, Integer> neighborhoodToCountMap = listingService.getNeighborhoodToListingCountMap();
 
-
+        //TODO:be This is a good way to condense the passage of data to view? Odd how the view must be rigid though with neighborhood names...revise
+        //Populate model with Neighborhood Listing Count attributes to pass to the view layer
+        for(Map.Entry<String, Integer> entry : neighborhoodToCountMap.entrySet()){
+            model.addAttribute(entry.getKey().replace("[ /]", "").toLowerCase(), entry.getValue());
+        }
 
         return "/index";
     }
