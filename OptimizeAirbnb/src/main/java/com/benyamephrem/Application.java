4e44d8ec54dd3cd15e71e706e2171c8aa279c3ce
Benyam Ephrem <ephremdsg@gmail.com>
2017-10-27 14:43:41 -0400

    Made 3 buttons on homepage and anchored footer to the bottom. Still looks really rough but will start on bonus anyway since I'm more focused on finishing the backend right now.

--- OptimizeAirbnb/src/main/java/com/benyamephrem/Application.java
+++ OptimizeAirbnb/src/main/java/com/benyamephrem/Application.java
@@ -12,3 +12,12 @@ public class Application{
     }
 
 }
+
+// TODO: Animate: Make frontend look halfway decent
+// TODO: Animate: Add an animation to your visualization.
+// TODO: Investment: If I have $100 million to invest, where in San Francisco should I buy properties so I can maximize my returns with Airbnb? When will I break even?
+// TODO: Popularity: Can you identify the neighborhood that averages the most positive reviews?
+// TODO: Cache database responses to save unnecessary queries
+// TODO: Consolidate excessive database queries
+// TODO: Clean up code verbosity and use Java 8 lambdas where you can
+// TODO: Write Unit Tests for DAO, Service, and Controller layers.
